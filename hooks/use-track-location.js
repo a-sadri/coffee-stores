f4b97096524785b999f1c667121f34597d903de1
Ali Sadri <asadri721@gmail.com>
2022-06-11 15:26:32 +0430

    Return user location

--- hooks/use-track-location.js
+++ hooks/use-track-location.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 const useTrackLocation = () => {
   const [locationErrorMsg, setLocationErrorMsg] = useState('');
   const [latLong, setLatLong] = useState('');
+  const [isFindingLocation, setIsFindingLocation] = useState(false);
 
   const success = (position) => {
     const latitude = position.coords.latitude;
@@ -10,15 +11,19 @@ const useTrackLocation = () => {
 
     setLatLong(`${latitude},${longitude}`);
     setLocationErrorMsg('');
+    setIsFindingLocation(false);
   };
 
   const error = () => {
     setLocationErrorMsg('Unable to retrieve your location');
+    setIsFindingLocation(false);
   };
 
   const handleTrackLocation = () => {
+    setIsFindingLocation(true);
     if (!navigator.geolocation) {
       setLocationErrorMsg('Geolocation is not supported by your browser');
+      setIsFindingLocation(false);
     } else {
       //   status.textContent = 'Locating…';
       navigator.geolocation.getCurrentPosition(success, error);
